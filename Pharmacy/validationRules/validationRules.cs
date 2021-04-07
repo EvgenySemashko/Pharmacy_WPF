595f6a3593a15e43fe95d4adf0c23547d50ee239
Семашко Евгений <ezarioexp@gmail.com>
2021-04-07 19:45:32 +0300

    old project

---
+++ Pharmacy/validationRules/validationRules.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Pharmacy
+{
+    class validationRules
+    {
+        public static bool IsPhoneNumber(string numberPhone)
+        {
+            return Regex.Match(numberPhone, @"^(\+[0-9]{9})$").Success;
+        }
+    }
+}
